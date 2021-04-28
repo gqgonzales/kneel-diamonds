cfd757d2295016ad885cd81ebbdec266b10d0224
Gabe Gonzales <gq.gonzales1@gmail.com>
2021-04-28 14:29:31 -0500

    Not registering new orders

--- scripts/database.js
+++ scripts/database.js
@@ -53,13 +53,35 @@ export const getOrders = () => {
 };
 
 export const setMetal = (id) => {
-  database.orderBuilder.metalId = id
-}
+  database.orderBuilder.metalId = id;
+};
 
 export const setSize = (id) => {
-  database.orderBuilder.sizeId = id
-}
+  database.orderBuilder.sizeId = id;
+};
 
 export const setStyle = (id) => {
-  database.orderBuilder.styleId = id
-}
+  database.orderBuilder.styleId = id;
+};
+
+// Book 4 Chapter 8
+
+export const addCustomOrder = () => {
+  // Copy the current state of user choices
+  const newOrder = { ...database.orderBuilder };
+
+  // Add a new primary key to the object
+  newOrder.id = [...database.customOrders].pop().id + 1;
+
+  // Add a timestamp to the order
+  newOrder.timestamp = Date.now();
+
+  // Add the new order object to custom orders state
+  database.customOrders.push(newOrder);
+
+  // Reset the temporary state for user choices
+  database.orderBuilder = {};
+
+  // Broadcast a notification that permanent state has changed
+  document.dispatchEvent(new CustomEvent("stateChanged"));
+};
